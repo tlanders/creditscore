bb9ad4a2f7b4611bd982bad39003deee5159b83b
Troy Landers <tlanders@landersconsulting.biz>
2021-12-28 07:42:07 -0600

    spring boot starter files

---
+++ src/test/java/com/lci/creditscore/CreditscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lci.creditscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
